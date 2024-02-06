6f97227082645ca6afc8cecfa9cfebe25921ae16
qkdk <86948395+qkdk@users.noreply.github.com>
2024-02-06 13:30:56 +0900

    [Silver II] Title: 마인크래프트, Time: 112 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -3,41 +3,36 @@ import sys, math
 input = sys.stdin.readline
 
 n, m, b = map(int, input().split())
-matrix = []
+heightCount = [0 for _ in range(257)]
 
 for _ in range(n):
-    matrix.append(list(map(int, input().split())))
+    for v in map(int, input().split()):
+        heightCount[v] += 1
 
-# 파는것은 2초 쌓는것은 1초이기 때문에 완탐을 진행해야한다.
-# 그리디로 하려다가 망함...
+
 def solve():
-    summ = b
-    for row in matrix:
-        summ += sum(row)
-
-    limit = math.floor(summ / (n * m))
-
     maxHeight = 0
     cost = float('inf')
-    for k in range(limit + 1):
+
+    for targetHeight in range(257):
         digCount = 0
         adCount = 0
 
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] > k:
-                    digCount += matrix[i][j] - k
-                elif matrix[i][j] < k:
-                    adCount += k - matrix[i][j]
+        for currentHeight in range(257):
+            if targetHeight > currentHeight:
+                adCount += heightCount[currentHeight] * (targetHeight - currentHeight)
+            elif currentHeight > targetHeight:
+                digCount += heightCount[currentHeight] * (currentHeight - targetHeight)
 
         # 평탄화에 성공하는 경우
         if digCount + b >= adCount:
             nCost = digCount * 2 + adCount
             if cost >= nCost:
                 cost = nCost
-                maxHeight = k
+                maxHeight = targetHeight
 
     return cost, maxHeight
+
 
 a, b = solve()
 print(a, b)
