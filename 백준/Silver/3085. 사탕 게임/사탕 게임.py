af5db43384714e0374d28bb776ff19c2e3628258
qkdk <86948395+qkdk@users.noreply.github.com>
2024-02-07 12:50:25 +0900

    [Silver II] Title: 사탕 게임, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -9,45 +9,34 @@ for _ in range(n):
     matrix.append(list(input().strip()))
 
 
-def check():
-    maxCount = 0
+def check(lst):
+    tmpCount = 1
+    maxCount = 1
 
-    for i in range(n):
-        tmpCount = 1
-        for j in range(1, n):
-            if matrix[i][j] == matrix[i][j - 1]:
-                tmpCount += 1
-            else:
-                maxCount = max(maxCount, tmpCount)
-                tmpCount = 1
-        maxCount = max(maxCount, tmpCount)
-
-    for i in range(n):
-        tmpCount = 1
-        for j in range(1, n):
-            if matrix[j][i] == matrix[j - 1][i]:
-                tmpCount += 1
-            else:
-                maxCount = max(maxCount, tmpCount)
-                tmpCount = 1
-        maxCount = max(maxCount, tmpCount)
+    for i in range(len(lst) - 1):
+        if lst[i] == lst[i + 1]:
+            tmpCount += 1
+            maxCount = max(maxCount, tmpCount)
+        else:
+            tmpCount = 1
 
     return maxCount
 
 
 def solve():
-    answer = check()
+    answer = 0
 
     for i in range(n):
         for j in range(n - 1):
             matrix[i][j], matrix[i][j + 1] = matrix[i][j + 1], matrix[i][j]
-            answer = max(answer, check())
+            answer = max(answer, check(matrix[i]), check([matrix[x][j] for x in range(n)]),
+                         check([matrix[x][j + 1] for x in range(n)]))
             matrix[i][j], matrix[i][j + 1] = matrix[i][j + 1], matrix[i][j]
 
     for i in range(n):
         for j in range(n - 1):
             matrix[j][i], matrix[j + 1][i] = matrix[j + 1][i], matrix[j][i]
-            answer = max(answer, check())
+            answer = max(answer, check(matrix[j]), check(matrix[j + 1]), check([matrix[x][i] for x in range(n)]))
             matrix[j][i], matrix[j + 1][i] = matrix[j + 1][i], matrix[j][i]
 
     return answer
