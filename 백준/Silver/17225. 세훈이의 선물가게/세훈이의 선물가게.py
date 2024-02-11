89655d947aca15fbf9f9adf81c6b816c6943a346
qkdk <86948395+qkdk@users.noreply.github.com>
2024-02-11 14:39:24 +0900

    [Silver I] Title: 세훈이의 선물가게, Time: 112 ms, Memory: 47840 KB, Score: 140 point -BaekjoonHub

--- 백준/Silver/17225. 세훈이의 선물가게/세훈이의 선물가게.py
+++ 백준/Silver/17225. 세훈이의 선물가게/세훈이의 선물가게.py
@@ -1,42 +1,45 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 a, b, n = map(int, input().split())
 
-bq = deque()
-rq = deque()
-
-aStack = []
 bStack = []
+rStack = []
 
 for _ in range(n):
     t, c, m = input().strip().split()
     t = int(t)
     m = int(m)
     if c == "B":
+        st = t
+        if bStack:
+            st = max(bStack[-1][0] + a, t)
         for i in range(m):
-            bq.append(t + i * a)
+            bStack.append((st + i * a, 0))
     else:
+        st = t
+        if rStack:
+            st = max(rStack[-1][0] + b, t)
         for i in range(m):
-            rq.append(t + i * b)
+            rStack.append((st + i * b, 1))
+
+bStack.extend(rStack)
+
+bStack.sort()
 
 count = 1
-second = 1
-while second < 86401:
-    if bq and bq[0] < second:
-        bq.popleft()
-        aStack.append(count)
-        count += 1
-    elif rq and rq[0] < second:
-        rq.popleft()
-        bStack.append(count)
+bbStack = []
+rrStack = []
+for v in bStack:
+    if v[1] == 0:
+        bbStack.append(str(count))
         count += 1
     else:
-        second += 1
+        rrStack.append(str(count))
+        count += 1
 
-print(len(aStack))
-print(' '.join(map(str, aStack)))
-print(len(bStack))
-print(' '.join(map(str, bStack)))
+print(len(bbStack))
+print(' '.join(bbStack))
+print(len(rrStack))
+print(' '.join(rrStack))
